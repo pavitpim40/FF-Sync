47043915f6f12b09323cd98046f937e29674911e
Pavit Pimchanagul <pavitpim@gmail.com>
2023-04-19 14:32:00 +0700

    push

--- srcs/app/app.js
+++ srcs/app/app.js
@@ -1,19 +1,25 @@
-const componentService = new ComponentService();
-const errorService = new ErrorService();
+const app = () => {
+    const componentService = new ComponentService();
+    const errorService = new ErrorService();
 
-const calTotal = () => {
-    errorService.hideError();
-    const inputs = componentService.getInputs();
-    const numbers = parseInput(...inputs); // [1,2,4] , ...[1,2,4] == 1,2,4
-    const valid = validateInputs(...numbers);
+    errorService.hideError(); //*** */
 
-    if (valid) {
-        const [price, quantity, shipping] = numbers;
-        const totalPrice = price * quantity + shipping;
-        componentService.showPrice(totalPrice);
-    } else {
-        errorService.showErrorMessage(inputs, numbers);
-    }
+    const calTotal = () => {
+        errorService.hideError();
+        const inputs = componentService.getInputs(); //*** */ ได้ array ของ input
+        const numbers = parseInput(...inputs); // [1,2,4] , ...[1,2,4] == 1,2,4
+        const valid = validateInputs(...numbers);
+
+        if (valid) {
+            const [price, quantity, shipping] = numbers;
+            const totalPrice = price * quantity + shipping;
+            componentService.showPrice(totalPrice); // Display ราคา
+        } else {
+            errorService.showErrorMessage(inputs, numbers); //*** */
+        }
+    };
+
+    componentService.onClick(calTotal); //*** */
 };
-// totalBtn.addEventListener('click', calTotal);
-componentService.onClick(calTotal);
+
+app();
